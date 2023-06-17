49934041474af22045e22c53f35e39a348669639
Ankit Sharma <an2ki4t5s@gmail.com>
2023-06-18 01:06:56 +0530

    made segment for loading things

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,21 @@
 import React,{useState, useEffect}from 'react';
+
 import { Loader , FormField , Cards } from '../componenets';
 
-
+const RenderCard = ({data,title}) =>{
+  if(data?.length>0) {
+    return data.map((post) => <Card key = {post._id} {...post} />)
+    }
+    return (
+      <h2 className="mt-5 font-bold text-[#6469ff] text-xl uppercase">
+        {title}
+      </h2>
+    )
+}
 const Home = () => {
-  const [loading,setLoading ] = useState(false);
-  const [allPosts,setAllPosts] = useState([nul]);
-
+  const [loading, setloading] = useState(false);
+  const [allPost, setAllpost] = useState(null);
+  const [searchText, setSearchText] = useState('');
   return (
     <section className="max-w-7xl mx-auto">
       <div>
@@ -29,10 +39,36 @@ const Home = () => {
           </div>
         )
       : ( <>
+      {searchText && (
+        <h2 className = "font-medium text-[#666e75] text-xl mb-3">
+          Showing results from <span className = " text-[#222328]">
+            {searchText}</span>
+
+
+        </h2>
+      )}
+      <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-col-2 grid-cols-1 gap-3 ">
+        {searchText ? ( 
+          <RenderCards 
+          data = {[]}
+          title = "No Results Found"
+          />
+        )
+        : (
+          <RenderCard 
+          data = {[]}
+          title= "No posts found" 
+          />
+        )}
+        
+
+      </div>
+
       </>)}
       </div>
 
     </section>
+
   )
 }
 
