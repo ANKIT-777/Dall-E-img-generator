281c6ac5988432ecd5481a6280614b80c4cf5e8f
Ankit Sharma <an2ki4t5s@gmail.com>
2023-06-17 23:53:31 +0530

    implemented loader and form at home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,38 @@
-import React,{useState, useEffect}from 'react'
+import React,{useState, useEffect}from 'react';
+import { Loader , FormField , Cards } from '../componenets';
+
 
 const Home = () => {
+  const [loading,setLoading ] = useState(false);
+  const [allPosts,setAllPosts] = useState([nul]);
+
   return (
-    <div>Home</div>
+    <section className="max-w-7xl mx-auto">
+      <div>
+        <h1 className="font-extrabold text-[#222328] text-[32px]">
+          The Community showcase
+        </h1>
+        <p className="mt-2 text-[#666e75] text-[14px] max-w-[500px]">
+          Browse the Community of the University  of Texas at Austin
+        </p>
+      </div>
+
+      <div className="mt-16">
+        <FormField/> 
+      
+      </div>
+
+      <div className="mt-10">
+        {loading ? (
+          <div className="flex justify-center items-center">
+            <Loader/>
+          </div>
+        )
+      : ( <>
+      </>)}
+      </div>
+
+    </section>
   )
 }
 
