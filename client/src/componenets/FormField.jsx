71a21f174a7881d645c767d4a1a74fd6cc160976
Ankit Sharma <an2ki4t5s@gmail.com>
2023-06-25 23:50:29 +0530

    updated form field and loader section

--- client/src/componenets/FormField.jsx
+++ client/src/componenets/FormField.jsx
@@ -1,31 +1,47 @@
 import React from 'react'
 
-const FormField = ({labelName, typem,name, placeholder,value,handleChange,isSurpriseMe,handleSurpriseMe}) => {
-  return (
+const FormField = ({
+  labelName,
+  type,
+  name,
+  placeholder,
+  value,
+  handleChange,
+  isSurpriseMe,
+  handleSurpriseMe,
+})  => (
+ 
     <div>
       <div className="flex items-center gap-2 mb-2">
-        <label htmlFor={name}
-        className="block text-sm font-medium text-gray-900"></label>
-        {isSurpriseMe && (
-           <button
-           type="button"
-           onClick={handleSurpriseMe}
-          className="font-semibold text-xs bg-[#ECECf1] py-1 px-2 rounded-[5px] text-black">
-            surpirse me 
-          </button>
-          )}
-      </div>
-      <input type={type}
-      id= {name}
-      name = {name}
+      <label
+        htmlFor={name}
+        className="block text-sm font-medium text-gray-900"
+      >
+        {labelName}
+      </label>
+      {isSurpriseMe && (
+        <button
+          type="button"
+          onClick={handleSurpriseMe}
+          className="font-semibold text-xs bg-[#EcECF1] py-1 px-2 rounded-[5px] text-black"
+        >
+          Surprise me
+        </button>
+      )}
+    </div>
+
+    <input
+      type={type}
+      id={name}
+      name={name}
+      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#6469ff] focus:border-[#6469ff] outline-none block w-full p-3"
       placeholder={placeholder}
       value={value}
       onChange={handleChange}
       required
-      className="bg-gray-50 border-gray-300 text-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#4649ff] focus:border-[#4649ff] outline-none block w-full p-3"  />
-      
-    </div>
-  )
-}
+    />
+  </div>
+  );
+
 
 export default FormField
