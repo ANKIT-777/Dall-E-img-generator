676328998a0c7eb7eac6c14d499fc3e1dd361727
Ankit Sharma <an2ki4t5s@gmail.com>
2023-06-27 20:34:19 +0530

    debugged all the errors in front end

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,8 +1,8 @@
-import {surprisemePormpts } from '../contants';
+import { surpriseMePrompts } from '../contants';
 
 export function getRandomPormpt(prompt){
-    const randomIndex = Math.floor(Math.random() * surprisemePormpts.length);
-    const randomPrompt = surprisemePormpts[randomIndex];
+    const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
+    const randomPrompt = surpriseMePrompts[randomIndex];
 
     if(randomPrompt === prompt) return getRandomPormpt(prompt); 
     return randomPrompt;
