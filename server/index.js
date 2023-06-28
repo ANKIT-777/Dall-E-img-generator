20c9c2d6b0086a5e35478b1c4645b7e0cc32f6ba
Ankit Sharma <an2ki4t5s@gmail.com>
2023-06-28 14:17:30 +0530

    completed the backend part and debugged

--- server/index.js
+++ server/index.js
@@ -1,42 +1,32 @@
 import express from 'express';
-import * as dotenv from 'dotenv';
-
+import dotenv from 'dotenv';
 import cors from 'cors';
 
 import connectDB from './mongoDb/connect.js';
-import postRoutes from './routes/postRoutes.js';
-import dalleRoutes from './routes/dalleRoutes.js';
+import postRoutes from './rotues/postRoutes.js';
+import dalleRoutes from './rotues/dalleRoutes.js';
 
 dotenv.config();
 
 const app = express();
 
 app.use(cors());
-app.use(express.json({limit : '50'}));
+app.use(express.json({ limit: '50mb' }));
 
-app.use('/api/v1/post',postRoutes);
-app.use('/api/v1/post',dalleRoutes); 
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/dalle', dalleRoutes);
 
 app.get('/', async (req, res) => {
-    res.send('hello world');
-})
+  res.send('hello world');
+});
 
-const startserver = async (req, res) => {
-    
+const startServer = async () => {
+  try {
+    await connectDB(process.env.MONGODB_URL);
+    app.listen(8080, () => console.log('Server has started on http://localhost:8080'));
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-   /* The commented code block is attempting to connect to a MongoDB database using the URL specified
-   in the environment variable `MONGODB_URL`. If the connection is successful, it starts the server
-   on port 8080 and logs a message to the console. If there is an error connecting to the database,
-   it logs the error to the console. However, the `startserver()` function is currently empty and
-   not being called anywhere, so this code block is not being executed. */
-    try {
-        connectDB(process.env.MONGODB_URL);
-        app.listen(8080, () => console.log('server has started on port http://localhost:8080'))
-    }
-    catch (error) {
-        console.error(error);
-    }
-    
-}
-
-startserver();
+startServer();
