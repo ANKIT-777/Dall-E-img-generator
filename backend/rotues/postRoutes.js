c0dd37e4820b7299c6c99b192b60ba8962bf066d
Ankit Sharma <an2ki4t5s@gmail.com>
2023-07-05 23:38:02 +0530

    done with post routes

--- backend/rotues/postRoutes.js
+++ backend/rotues/postRoutes.js
@@ -8,4 +8,18 @@ dotenv.config();
 
 const router = express.Router();
 
+cloudinary.config(
+    {
+        cloud_name: process.env.API_KEY,
+        api_key: process.env.API_KEY,
+        api_secret: process.env.API_SECRET,
+
+    }
+);
+
+router.route('/').post(async(req,res) => {
+    const { name , prompt, photo } = req.body; 
+    cont photoUrl = await cloudinary
+})
+
 export default router;
