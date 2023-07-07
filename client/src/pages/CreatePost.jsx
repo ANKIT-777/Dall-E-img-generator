c8d931111e528192f80b4f5d787eb44c640441b1
Ankit Sharma <an2ki4t5s@gmail.com>
2023-07-07 19:21:22 +0530

    mixed post- functionality and integrated with home

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -43,9 +43,34 @@ const CreatePost = () => {
         }
       };
       
-      const handleSubmit = () => {
+      const handleSubmit = async (e) => {
+        e.preventDefault(); 
+        if(form.prompt && form.photo){
+          setLoading(true);
+          try {
+            const response = await fetch('http://localhost:8080/api/v1/post',{
+            method : 'POST',
+            headers : { 'content-type': 'application/json',
+
+            },
+            body : JSON.stringify(form)
+          })
+          await response.json();
+          navigate('/');
 
         }
+        catch (error) {
+          alert(error)
+        }
+        finally {
+          setLoading(false);
+        }
+
+        }
+        else{
+          alert('please enter a prompt for generating a new image');
+        }
+      }
 
         const handleChange = (e) => {
           setForm({...form,[e.target.name]: e.target.value})
