5e88fb9cc16b3105d1b81cccf86659a1809e5013
Ankit Sharma <an2ki4t5s@gmail.com>
2023-07-07 18:12:54 +0530

    added cloudinary - posting pictures and uploading

--- backend/rotues/postRoutes.js
+++ backend/rotues/postRoutes.js
@@ -7,5 +7,42 @@ import Post from '../mongoDb/models/post.js';
 dotenv.config();
 
 const router = express.Router();
+cloudinary.config({
+    cloud_name : process.env.CLOUDINARY_CLOUD_NAME,
+    api_key : process.env.CLOUDINARY_API_KEY,
+    api_secret : process.env.CLOUDINARY_API_SECRET,
+});
+//get all the post
+router.route('/').get(async(req,res) => {
+    try {
+        const posts = await Post.find({});
+    res.status(200).json({ success: true, message: posts})
+        
+    }
+    catch (err) {
+        res.status(500).json({ success: false, message: err});
+    }
+
+});
+
+router.route('/').post(async(req,res) => {
+    try {
+    const {name , prompt, photo } = req.body; 
+    const photoUrl = await cloudinary.uploader.upload(photo);
+    const newPost = await Post.create({
+        name, 
+        prompt, 
+        photoU : photoUrl.url,
+    })
+
+    res.status(201).json({ success: true, data: newPost });
+
+}
+catch (err) {
+    res.status(500).json({ success: false , message: err});
+}
+
+});
+
 
 export default router;
